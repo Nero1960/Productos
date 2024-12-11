c7029977348ae7b58e4c35fc2988746f2a555184
Andy <menaandy198@gmail.com>
2024-12-11 17:47:36 -0600

    Eliminando el frontend

--- client/src/types/index.ts
+++
@@ -1,7 +0,0 @@
-import {productSchema, ProductSchema} from '../schema/newProduct.schema'
-import {z} from 'zod'
-
-
-export type product = z.infer<typeof productSchema>;
-
-export type ProductType = z.infer<typeof ProductSchema>;
--- client/src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-export function formatCurrency( amount : number){
-
-    return new Intl.NumberFormat('en-US', {
-        currency: 'USD',
-        style: 'currency'
-    }).format(amount)
-
-}
