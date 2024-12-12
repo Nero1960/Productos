f6e52dc706f3d55cca8a0bf3339f7589a55850e9
Andy <menaandy198@gmail.com>
2024-12-11 19:18:38 -0600

    Configurando puerto

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -5,7 +5,6 @@ dotenv.config()
 
 const database = new Sequelize(process.env.DATABASE_URI, {
     dialect: 'mysql',
-    port: 3306,
     models: [__dirname + '/../models/**/*.ts'],
     logging: false
 })
